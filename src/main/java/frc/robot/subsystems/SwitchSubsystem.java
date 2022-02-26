c61d48a110587b15c4caee18091bba59f3a77841
FR3HDY <94580281+FR3HDY@users.noreply.github.com>
2022-02-26 15:47:04 -0500

    Create switch subsystem

---
+++ src/main/java/frc/robot/subsystems/SwitchSubsystem.java
@@ -0,0 +1,35 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+public class SwitchSubsystem extends SubsystemBase {
+  private DigitalInput limitSwitch;
+
+  /** Creates a new SwithSubsytem. */
+  public SwitchSubsystem(int switchPort) {
+    // Initialize the subsystem if the shooter exists
+    if (switchPort != -1) {
+      limitSwitch = new DigitalInput(switchPort);
+    }
+    else
+    {
+      limitSwitch = null;
+    }
+
+  }
+  public boolean isNotActive() {
+    return limitSwitch != null;
+}
+  public boolean isDetected(){
+    return limitSwitch.get();
+    //to double check the value of get
+  }
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
