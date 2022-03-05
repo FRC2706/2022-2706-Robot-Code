021762e454c0cd4593e7f0aa1f2e5ba3942680ff
Patrick He <77468352+phetheep@users.noreply.github.com>
2022-03-05 16:20:07 -0500

    Create index cargo command

---
+++ src/main/java/frc/robot/commands/IndexerCargo.java
@@ -0,0 +1,83 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.config.Config;
+import frc.robot.subsystems.ColorSensorSubsystem;
+import frc.robot.subsystems.IndexerSubSystem;
+import frc.robot.subsystems.SwitchSubsystem;
+
+public class IndexerCargo extends CommandBase {
+
+  private IndexerSubSystem indexer;
+  private ColorSensorSubsystem colorSensor;
+  private SwitchSubsystem switchDetector;
+  public boolean switchDetected;
+  public boolean colorSensorDetected;
+  public boolean colorSensorFirstDetected = false;
+  public int counter = 0;
+
+  /** Creates a new IndexerCargo. */
+  public IndexerCargo() {
+
+    indexer = IndexerSubSystem.getInstance();
+    colorSensor = ColorSensorSubsystem.getInstance();
+    switchDetector = new SwitchSubsystem(Config.INDEXER_SWITCH);
+
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(indexer);
+    addRequirements(colorSensor);
+    addRequirements(switchDetector);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+
+    indexer.setIndexerPosition();
+  }
+    
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    
+    switchDetected = switchDetector.isDetected();
+
+    if (switchDetected == true) {
+      indexer.stop();
+    } else {
+      colorSensorDetected = colorSensor.isDetected();
+      if (colorSensorDetected == true && colorSensorFirstDetected == false) {
+        colorSensorFirstDetected = true;
+
+      }
+      if (colorSensorFirstDetected == true) {
+        
+        indexer.runForIntake();
+        counter++;
+        
+        // TODO tune for 100
+        if (counter > 100) {
+          // At this time, the cargo should be in the indexer, and the indexer should stop
+          colorSensorFirstDetected = false;
+        }
+      } else {
+        indexer.stop();
+      }
+    }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+
+
+}
