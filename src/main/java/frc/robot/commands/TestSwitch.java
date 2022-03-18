2f01871ffbbd5444f8d6ee1dd90c285e6333d3c6
davidliu-2706 <dliu5@ocdsb.ca>
2022-03-18 12:20:02 -0400

    Fixed with limit switch change

--- src/main/java/frc/robot/commands/TestSwitch.java
+++ src/main/java/frc/robot/commands/TestSwitch.java
@@ -26,7 +26,7 @@ public class TestSwitch extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    if( switchDetector.isDetected() == true )
+    if( switchDetector.getResult() == true )
     {
       System.out.println("Switch detected");
     }
