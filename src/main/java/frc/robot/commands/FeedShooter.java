56f060ca2ca9a0cbc068dd1353bcacb348be41c4
ErikCald <eeejjj8@gmail.com>
2022-03-29 00:40:27 -0400

    Indexing and Feeding Commands
    DetectCargoAtIndexer will be the default command to check the colour sensor for a cago and schedule IndexCargoOnce.
    
    IndexCargoOnce is a statemachine like command to index either cargo.
    
    FeedShooter will feed the shooter in a very controlled manner, only feeding when the Shooter motor is at the target RPM.

---
+++ src/main/java/frc/robot/commands/FeedShooter.java
@@ -0,0 +1,107 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.config.Config;
+import frc.robot.config.FluidConstant;
+import frc.robot.subsystems.IndexerSubSystem;
+import frc.robot.subsystems.ShooterSubsystem;
+import frc.robot.subsystems.SwitchSubsystem;
+
+public class FeedShooter extends CommandBase {
+
+    private static final FluidConstant<Integer> feedingSpeedRPM = new FluidConstant<>("Feeding RPM", 700)
+    .registerToTable(Config.constantsTable);
+
+    private int feedingState = 0;
+
+    /** Creates a new feedShooter. */
+    public FeedShooter() {
+        addRequirements(IndexerSubSystem.getInstance());
+    }
+
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+        switch (IndexerSubSystem.getInstance().numCargo) {
+            case 2:
+                feedingState = 6;
+            case 1: 
+                feedingState = 3;
+            case 0:
+                this.cancel();
+        }
+    }
+
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        switch (feedingState) {
+            /** Start here for two cargo  **/
+            // Feed until cargo is aligned at the second switch
+            case 6:
+                if (SwitchSubsystem.getInstance().getSecondSwitch()) {
+                    feedingState--;
+                } else {
+                    IndexerSubSystem.getInstance().setTargetRPM(feedingSpeedRPM.get());
+                }
+
+            // Wait for shooter targetRPM
+            case 5:
+                if (ShooterSubsystem.getInstance().isAtTargetRPM()) {
+                    feedingState--;
+                }
+            
+            // Feed cargo into shooter until second switch is unpressed
+            case 4: 
+                if (SwitchSubsystem.getInstance().getSecondSwitch()) {
+                    IndexerSubSystem.getInstance().setTargetRPM(feedingSpeedRPM.get());
+                } else {
+                    IndexerSubSystem.getInstance().stop();
+                    IndexerSubSystem.getInstance().numCargo = 1;
+                    feedingState--;
+                }
+
+            /** Start here for one cargo  **/
+            // Move cargo until it hits the second switch
+            case 3:
+                if (SwitchSubsystem.getInstance().getSecondSwitch()) {
+                    feedingState--;
+                } else {
+                    IndexerSubSystem.getInstance().setTargetRPM(feedingSpeedRPM.get());
+                }
+
+            
+            // Wait for shooter targetRPM
+            case 2:
+                if (ShooterSubsystem.getInstance().isAtTargetRPM()) {
+                    feedingState--;
+                }
+            
+            // Feed cargo until switch is unpressed, then stop this command
+            case 1:
+                if (SwitchSubsystem.getInstance().getSecondSwitch()) {
+                    IndexerSubSystem.getInstance().setTargetRPM(feedingSpeedRPM.get());
+                } else {
+                    IndexerSubSystem.getInstance().stop();
+                    IndexerSubSystem.getInstance().numCargo = 0;
+                    this.cancel();
+                }
+        }
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+        IndexerSubSystem.getInstance().stop();
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return IndexerSubSystem.getInstance().numCargo == 0;
+    }
+}
---
+++ src/main/java/frc/robot/commands/IndexCargoOnce.java
@@ -0,0 +1,83 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.IndexerSubSystem;
+import frc.robot.subsystems.SwitchSubsystem;
+
+public class IndexCargoOnce extends CommandBase {
+
+    /**
+     * CONSTANTS
+     */
+    private final int zeroCargoIncrementPastSwitch = 10;
+    private final int rpmForIndexing = 700;
+
+
+    
+    private boolean reachedFirstLimitSwitch = false;
+    private int numCargoAtBeginning = 0;
+
+    /** Creates a new IndexCargoOnce. */
+    public IndexCargoOnce() {
+        addRequirements(IndexerSubSystem.getInstance());
+    }
+
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+        numCargoAtBeginning = IndexerSubSystem.getInstance().numCargo;
+        switch (numCargoAtBeginning) {
+            case 0:
+                reachedFirstLimitSwitch = false;
+            
+            case 1:
+                IndexerSubSystem.getInstance().numCargo++;
+        }
+    }
+
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        switch (numCargoAtBeginning) {
+            case 0:
+                if (reachedFirstLimitSwitch == false) {
+                    if (SwitchSubsystem.getInstance().getFirstSwitch()) {
+                        reachedFirstLimitSwitch = true;
+                        IndexerSubSystem.getInstance().numCargo++; 
+                        IndexerSubSystem.getInstance().setIndexerPosition();
+                    } else {
+                        IndexerSubSystem.getInstance().setTargetRPM(rpmForIndexing);
+                    }
+                    
+                }
+                if (reachedFirstLimitSwitch == true) {
+                    IndexerSubSystem.getInstance().runForIntake(zeroCargoIncrementPastSwitch);
+                }
+
+            case 1:
+                IndexerSubSystem.getInstance().setTargetRPM(rpmForIndexing);   
+        }
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+        IndexerSubSystem.getInstance().stop();
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        if (SwitchSubsystem.getInstance().getSecondSwitch()) {
+            return true;
+        }
+        else if (numCargoAtBeginning == 0 && IndexerSubSystem.getInstance().isAtTargetPosition()) {
+            return true;
+        }
+        return false;
+    }
+}
